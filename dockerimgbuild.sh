02d466f118568f126f6a5348a81cbc10e3b99157
BhargavSellam <147702067+BhargavSellam@users.noreply.github.com>
2023-11-08 17:21:17 +0530

    Create dockerimgbuild.sh

---
+++ dockerimgbuild.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ 
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/e3l3f0q4
+docker build -t java-app .
+docker tag java-app:latest public.ecr.aws/e3l3f0q4/java-app:latest
+docker push public.ecr.aws/e3l3f0q4/java-app:latest
